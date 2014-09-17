f0c8e9ab9ee59e0934ac4264395a80dc7726886b
temarez <temarez@gmail.com>
2014-09-17 16:24:51 +0300

    A "Front Controller" to the Rescue

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
+// index.php
+
+// Загружаем и инициализируем глобальные библиотеки
 require_once 'model.php';
+require_once 'controllers.php';
 
-$posts = get_all_posts();
-
-require 'templates/list.php';
+// Внутренняя маршрутизация
+$uri = $_SERVER['REQUEST_URI'];
+if ($uri == '/index.php') {
+    list_action();
+} elseif ($uri == '/index.php/show' && isset($_GET['id'])) {
+    show_action($_GET['id']);
+} else {
+    header('Status: 404 Not Found');
+    echo '<html><body><h1>Page Not Found</h1></body></html>';
+}
